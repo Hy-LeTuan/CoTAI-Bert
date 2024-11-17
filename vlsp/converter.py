a2e57492dc4deb972e88a86fb993bcd826b7b089
Hy-LeTuan <letuanhy1507@gmail.com>
2024-11-17 14:49:41 -0600

    update converter

--- vlsp/converter.py
+++ vlsp/converter.py
@@ -1,6 +1,4 @@
-import torch
 from glob import glob
-import os
 
 
 # read file and extract tags
@@ -14,7 +12,7 @@ def extract_and_reformat(text_file_paths, destination):
                 f"./data/formatted_data/2016/{destination}/{str(file_counter).zfill(5)}.csv", "w", encoding="utf-8")
 
             # write header
-            output_file.write(f"Word,Tag\n")
+            output_file.write("Word,Tag\n")
 
             line_counter = 0
             while content:
@@ -42,6 +40,57 @@ def extract_and_reformat(text_file_paths, destination):
             output_file.close()
 
 
+def read_and_join_list(text_file_paths, destination):
+    for file_counter, filepath in enumerate(text_file_paths):
+        with open(filepath, "r", encoding="utf-8") as input_file:
+            output_file = open(
+                f"./data/formatted_data/2016/new/{destination}/{str(file_counter).zfill(5)}.txt",
+                "w",
+                encoding="utf-8"
+            )
+
+            content = input_file.readline()
+            line_counter = 0
+
+            while content:
+                if line_counter < 3:
+                    line_counter += 1
+                    content = input_file.readline()
+                else:
+                    # strip trailing space
+                    content = content.strip()
+
+                    if content == "<s>":
+                        content = input_file.readline()
+                        content = content.strip()
+
+                        # create word list and tag list
+                        word_list = []
+                        tag_list = []
+
+                        while content.strip() != "</s>":
+                            content = content.split("\t")
+                            word = content[0]
+                            tag = content[3]
+
+                            # replace quotation with double quotations
+                            word = word.replace('"', '""')
+
+                            word_list.append(word)
+                            tag_list.append(tag)
+
+                            # continue reading file
+                            content = input_file.readline()
+
+                        # write output to a file
+                        word_list_concat = " ".join(word_list)
+                        tag_list_concat = " ".join(tag_list)
+
+                        output_file.write(f"{word_list_concat}\t{tag_list_concat}\n")
+                    else:
+                        content = input_file.readline()
+
+
 if __name__ == "__main__":
     # get all .txt file paths in NER 2016 dataset
 
@@ -51,4 +100,5 @@ if __name__ == "__main__":
 
     for o, d in zip(origin, destination):
         text_file_paths = sorted(glob(o, recursive=True))
-        extract_and_reformat(text_file_paths, destination=d)
+        # extract_and_reformat(text_file_paths, destination=d)
+        read_and_join_list(text_file_paths=text_file_paths, destination=d)
