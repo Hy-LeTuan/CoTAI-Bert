38c02e1a94b395241fc60c239c32ab82ab14a771
Hy-LeTuan <letuanhy1507@gmail.com>
2024-11-20 22:45:53 -0600

    update data for VLSP

--- vlsp/tokenize_dataframe.py
+++ vlsp/tokenize_dataframe.py
@@ -4,14 +4,16 @@ from datasets import Dataset
 from glob import glob
 import os
 
-dataset_type = "train"
+
+dataset_type = "test"
 text_files = sorted(glob(os.path.join("./data/formatted_data/2016/new", dataset_type, "**", "*.txt"), recursive=True))
 tokenizer = AutoTokenizer.from_pretrained("../tokenizer/trained_tokenizer/tokenizer-50k/")
+tokenizer.model_max_length = 10000000
 
 
-def mlm_tokenize_dataset(batch):
-    word_list = batch["words"]
-    tokenizer_output = tokenizer(word_list, is_split_into_words=True, return_tensors="pt")
+def mlm_tokenize_dataset(row):
+    word_list = row["words"]
+    tokenizer_output = tokenizer(word_list, is_split_into_words=True, return_tensors="pt", truncation=False)
 
     return tokenizer_output
 
@@ -20,5 +22,5 @@ if __name__ == "__main__":
     df = read_and_return_df(text_files=text_files)
     ds = Dataset.from_spark(df, load_from_cache_file=False)
 
-    ds = ds.map(mlm_tokenize_dataset, batched=True, num_proc=os.cpu_count())
-    ds.save_to_disk("./data/mlm_data/2016/train")
+    ds = ds.map(mlm_tokenize_dataset, batched=False, num_proc=os.cpu_count())
+    ds.save_to_disk(f"./data/mlm_data/2016/{dataset_type}")
